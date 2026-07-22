4be363215de761c3baa3043d302fae8f97b9fb1e
Ravikant Pandit <ravikantpandit@Ravikants-MacBook-Air.local>
2026-07-22 22:58:43 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Room-Availability-Checker/',
   server: {
     port: 5173,
     open: true,
